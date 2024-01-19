64c47a7e239f672e81d26b9f7dd0005be65c140d
JasonNelson93 <jason_nelson@midco.net>
2024-01-19 09:26:36 -0600

    Update Product Management Features in SportsPro
    Changes include:
    - Added CRUD operations for products in ProductController.
    - Implemented data validation in the Product model.
    - Updated List, Edit, and Delete views for better product management.

---
+++ SportsPro/Controllers/ProductController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsPro.Models;
+
+namespace SportsPro.Controllers
+{
+    public class ProductController : Controller
+    {
+        private SportsProContext context { get; set; }
+
+        public ProductController(SportsProContext ctx) => context = ctx;
+
+        public IActionResult List()
+        {
+            var products = context.Products.OrderBy(p => p.ReleaseDate).ToList();
+            return View(products);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            ViewBag.Action = "Add";
+            return View("Edit", new Product());
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            ViewBag.Action = "Edit";
+            var product = context.Products.Find(id);
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                if (product.ProductID == 0)
+                {
+                    context.Products.Add(product);
+                    context.SaveChanges();
+                }
+                else
+                {
+                    context.Update(product);
+                    context.SaveChanges();
+                }  
+                return RedirectToAction("List", "Product");
+            }
+            else
+            {
+                ViewBag.Action = (product.ProductID == 0) ? "Add" : "Edit";
+                return View(product);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var product = context.Products.Find(id);
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Product product)
+        {
+            context.Products.Remove(product);
+            context.SaveChanges();
+            return RedirectToAction("List", "Product");
+        }
+    }
+}
--- SportsPro/Models/Product.cs
+++ SportsPro/Models/Product.cs
@@ -1,14 +1,25 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SportsPro.Models
 {
     public class Product
     {
-		public int ProductID { get; set; }
-		public string ProductCode { get; set; } = string.Empty;
-		public string Name { get; set; } = string.Empty;
-		[Column(TypeName = "decimal(8,2)")]
-		public decimal YearlyPrice { get; set; }
-		public DateTime ReleaseDate { get; set; } = DateTime.Now;
-	}
+        public int ProductID { get; set; }
+
+        [Required(ErrorMessage = "Please enter a product code.")]
+        public string ProductCode { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please enter a name.")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please enter a yearly price.")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Yearly price must be greater than $0.")]
+        [RegularExpression(@"^\d+(\.\d{1,2})?$", ErrorMessage = "Please enter a valid number for the yearly price.")]
+        [Column(TypeName = "decimal(8,2)")]
+        public decimal YearlyPrice { get; set; }
+
+        [Required(ErrorMessage = "Please enter a date.")]
+        public DateTime ReleaseDate { get; set; } = DateTime.Now;
+    }
 }
