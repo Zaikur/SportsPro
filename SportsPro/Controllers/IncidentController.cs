c079f98cf40ecb225bebec046e1b9b0e792d1799
Ayden Hofts <ayden.hofts@southeasttech.edu>
2024-01-23 02:09:37 -0600

    Added Incident controller, model, and incident views

---
+++ SportsPro/Controllers/IncidentController.cs
@@ -0,0 +1,96 @@
+﻿/*
+ * Ayden Hofts
+ * 01/22/2024
+ * This is the controller for the incident section of the website
+ */
+
+
+
+using Microsoft.AspNetCore.Mvc;
+using SportsPro.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+
+namespace SportsPro.Controllers
+
+{
+    public class IncidentController : Controller
+    {
+        private SportsProContext context { get; set; }
+
+        public IncidentController(SportsProContext ctx) => context = ctx;
+
+        public IActionResult Index()
+        {
+            return RedirectToAction("List");
+        }
+
+
+        public IActionResult List()
+        {
+            var incidents = context.Incidents
+                .OrderBy(i => i.IncidentID).ToList();
+            return View(incidents);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            // Display the Add/Edit Incident page with blank fields
+            ViewBag.Action = "Add";
+            return View("Edit", new Incident());
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            // Display the Add/Edit Incident page with the data for the selected incident
+            ViewBag.Action = "Edit";
+            var incident = context.Incidents.Find(id);
+            return View("Edit", incident);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Incident incident)
+        {
+            // Handle the form submission for adding/editing incidents
+            if (ModelState.IsValid)
+            {
+                if (incident.IncidentID == 0)
+                {
+                    context.Incidents.Add(incident);
+                }
+                else
+                {
+                    context.Update(incident);
+                }
+
+                context.SaveChanges();
+                return RedirectToAction("List", "Incident");
+            }
+            else
+            {
+                ViewBag.Action = (incident.IncidentID == 0) ? "Add" : "Edit";
+                return View("Edit", incident);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            // Display the Delete Incident page that confirms the deletion
+            var incident = context.Incidents.Find(id);
+            return View(incident);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Incident incident)
+        {
+            // Handle the form submission for deleting incidents
+            context.Incidents.Remove(incident);
+            context.SaveChanges();
+            return RedirectToAction("List", "Incident");
+        }
+    }
+}
--- SportsPro/Models/Incident.cs
+++ SportsPro/Models/Incident.cs
@@ -1,20 +1,42 @@
-﻿namespace SportsPro.Models
+﻿/*
+ * Ayden Hofts
+ * 01/22/2024
+ * This is the model for the incident section of the website
+ */
+
+
+using System.ComponentModel.DataAnnotations;
+
+
+namespace SportsPro.Models
+
+
 {
     public class Incident
     {
 		public int IncidentID { get; set; }
-		public string Title { get; set; } = string.Empty;
+        
+		[Required(ErrorMessage = "Please select a title.")]
+        public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please select a description.")]
         public string Description { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please enter the date opened.")]
+        [DataType(DataType.Date)]
         public DateTime DateOpened { get; set; } = DateTime.Now;
         public DateTime? DateClosed { get; set; } = null;
 
-		public int CustomerID { get; set; }                   // foreign key property
-		public Customer Customer { get; set; } = null!;       // navigation property
+        [Required(ErrorMessage = "Please select a Customer.")]
+        public int CustomerID { get; set; }                   // foreign key property
+        public Customer Customer { get; set; } = null!;       // navigation property
 
-		public int ProductID { get; set; }                    // foreign key property
-		public Product Product { get; set; } = null!;         // navigation property
+        [Required(ErrorMessage = "Please select a Product.")]
+        public int ProductID { get; set; }                    // foreign key property
+        public Product Product { get; set; } = null!;         // navigation property
 
-		public int TechnicianID { get; set; }                 // foreign key property 
+        [Required(ErrorMessage = "Please select a Technician.")]
+        public int TechnicianID { get; set; }                 // foreign key property 
 		public Technician Technician { get; set; } = null!;   // navigation property
 
 		
