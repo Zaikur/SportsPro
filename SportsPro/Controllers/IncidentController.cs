8fcff6844b49a0be5bf4858cc46c4102485b4d3b
Ayden Hofts <ayden.hofts@southeasttech.edu>
2024-01-29 19:55:15 -0600

    Modify and delete some comments

--- SportsPro/Controllers/IncidentController.cs
+++ SportsPro/Controllers/IncidentController.cs
@@ -23,18 +23,11 @@ namespace SportsPro.Controllers
         private List<Technician> technicians;
         private List<Customer> customers;
 
-        /*Expand the controller to include the lists - Quinton
-         * 
-         * 
-         * public IncidentController(SportsProContext ctx) => context = ctx;
-         */
-
         public IncidentController(SportsProContext ctx)
         {
             context = ctx;
 
             //Populate the lists with data from the database to send to the views
-            //Not having this and sending them to the view is why SelectList wasn't working for you - Quinton
             products = context.Products.ToList();
             technicians = context.Technicians.ToList();
             customers = context.Customers.ToList();
@@ -58,10 +51,6 @@ namespace SportsPro.Controllers
         {
             // Display the Add/Edit Incident page with blank fields
             ViewBag.Action = "Add";
-
-
-
-            //Send the lists to the view using the ViewBag - Quinton
             ViewBag.Customers = customers;
             ViewBag.Technicians = technicians;
             ViewBag.Products = products;
@@ -76,17 +65,11 @@ namespace SportsPro.Controllers
             // Display the Add/Edit Incident page with the data for the selected incident
             ViewBag.Action = "Edit";
 
-            //Instead of only including the incident -Q
-            // var incident = context.Incidents.Find(id);
-
-            //Include all foreign key information, and then filter by the id given - Quinton
             var incident = context.Incidents
                 .Include(c => c.Customer)
                 .Include(c => c.Technician)
                 .Include(c => c.Product)
                 .FirstOrDefault(c => c.IncidentID == id);
-
-
 
             //Add the lists to the ViewBag for use in the View on the SelectList methods
             ViewBag.Customers = customers;
@@ -113,29 +96,14 @@ namespace SportsPro.Controllers
                 }
 
                 context.SaveChanges();
-
-                //Incident shouldn't be in the Redirect - Quinton
-                //return RedirectToAction("List", "Incident");
-
                 return RedirectToAction("List");
             }
             else
             {
-                /*Not needed because if the ModelState.IsValid check fails, the same 'Edit' view is returned to the user
-                 *Because the view was initially rendered for edititing there is no need to change the Action value as it will remain the same.
-                 *-Quinton
-                 */
-                //ViewBag.Action = (incident.IncidentID == 0) ? "Add" : "Edit";
-
-                //Also include all necessary lists here
                 ViewBag.Customers = customers;
                 ViewBag.Technicians = technicians;
                 ViewBag.Products = products;
 
-                //No need to send the incident again, same reason as above. -Quinton
-                //return View("Edit", incident);
-
-                //Only return the view
                 return View("Edit");
             }
         }
