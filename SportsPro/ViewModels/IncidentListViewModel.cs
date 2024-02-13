8780c62ca6a4809d779ff1a97fbc4341ee01dfb8
Quinton Nelson <124801789+Zaikur@users.noreply.github.com>
2024-02-13 15:23:39 -0600

    Added @RenderSection to render validation scripts only in necessary files, targetted that section within the Customer and Incident Add/Edit views.

---
+++ SportsPro/ViewModels/IncidentListViewModel.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Quinton Nelson
+ * 02/08/2024
+ * This class defines a model of data that is needed by the 'List' view of the Incident manager
+ */
+
+using SportsPro.Models;
+
+namespace SportsPro.ViewModels
+{
+    public class IncidentListViewModel
+    {
+        public List<Incident> Incidents {  get; set; }
+        public string IncidentFilter { get; set; }
+
+        //Also include lists of Customers, Products, and Technicians that the View will need
+        public List<Customer> Customers { get; set; }
+        public List<Product> Products { get; set; }
+        public List<Technician> Technicianes { get; set; }
+    }
+}
