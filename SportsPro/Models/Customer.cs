124fa33f84a28f424b2bbca3f51b73023fb7eddd
Ayden Hofts <ayden.hofts@southeasttech.edu>
2024-04-13 18:45:51 -0500

    commit

--- SportsPro/Models/Customer.cs
+++ SportsPro/Models/Customer.cs
@@ -4,13 +4,8 @@
  * Added an ICollection for Registration
  */
 
-/*
- * Ayden Hofts
- * 04/13/2024
- * Added improved validation for the edit customer page
- */
-
 using System.ComponentModel.DataAnnotations;
+using SportsPro.Models.Attributes;
 
 namespace SportsPro.Models
 {
@@ -18,39 +13,36 @@ namespace SportsPro.Models
     {
         public int CustomerID { get; set; }
 
-        [Required(ErrorMessage = "Required.")]
-        [StringLength(50, ErrorMessage = "First name must be between 1 and 50 characters.", MinimumLength = 1)]
+        [Required(ErrorMessage = "First name is required.")]
+        [StringLength(51, MinimumLength = 1)]
         public string FirstName { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "Required.")]
-        [StringLength(50, ErrorMessage = "Last name must be between 1 and 50 characters.", MinimumLength = 1)]
+        [Required(ErrorMessage = "Last name is required.")]
+        [StringLength(51, MinimumLength = 1)]
         public string LastName { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "Required.")]
-        [StringLength(50, ErrorMessage = "Address must be between 1 and 50 characters.", MinimumLength = 1)]
+        [Required(ErrorMessage = "Address is required.")]
+        [StringLength(51, MinimumLength = 1)]
         public string Address { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "Required.")]
-        [StringLength(50, ErrorMessage = "City must be between 1 and 50 characters.", MinimumLength = 1)]
+        [Required(ErrorMessage = "City is required.")]
+        [StringLength(51, MinimumLength = 1)]
         public string City { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "Required.")]
-        [StringLength(50, ErrorMessage = "State must be between 1 and 50 characters.", MinimumLength = 1)]
+        [Required(ErrorMessage = "State is required.")]
+        [StringLength(51, MinimumLength = 1)]
         public string State { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "Required.")]
-        [StringLength(20, ErrorMessage = "Postal code must be between 1 and 21 characters.", MinimumLength = 1)]
-        [RegularExpression(@"^\d{5}(-\d{4})?$", ErrorMessage = "Invalid ZIP code format.")]
+        [Required(ErrorMessage = "Postal code is required.")]
+        [StringLength(21, MinimumLength = 1)]
         public string PostalCode { get; set; } = string.Empty;
 
         [Phone(ErrorMessage = "Invalid phone format.")]
-        [RegularExpression(@"^\(?\d{3}\)?[-\s]?\d{3}-\d{4}$", ErrorMessage = "Phone number must be in the (999) 999-9999 format.")]
+        [RegularExpression(@"^\d{3}-\d{3}-\d{4}$", ErrorMessage = "Invalid phone format. Use ###-###-####.")]
         public string? Phone { get; set; }
 
-        [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
-        [DataType(DataType.EmailAddress, ErrorMessage = "Please enter a valid email address.")]
-        [StringLength(50, ErrorMessage = "Email must be between 1 and 50 characters.", MinimumLength = 1)]
-
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        [EmailDuplicate]
         public string? Email { get; set; }
 
         [Required(ErrorMessage = "Please select a country.")]
