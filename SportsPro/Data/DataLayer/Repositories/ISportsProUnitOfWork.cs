d92ffb4a1d03f27089bdebd2b0b1c68107296335
Quinton Nelson <124801789+Zaikur@users.noreply.github.com>
2024-04-14 14:06:53 -0500

    Added UnitOfWork files

---
+++ SportsPro/Data/DataLayer/Repositories/ISportsProUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using NuGet.Packaging.Core;
+using SportsPro.Models;
+
+namespace SportsPro.Data.DataLayer.Repositories
+{
+    public class ISportsProUnitOfWork
+    {
+        // Read-only properties for each repository
+        IRepository<Customer> Customers { get; }
+        IRepository<Incident> Incidents { get; }
+        IRepository<Product> Products { get; }
+        IRepository<Registration> Registrations { get; }
+        IRepository<Technician> Technicians { get; }
+        IRepository<Country> Countries { get; }
+    }
+}
---
+++ SportsPro/Data/DataLayer/Repositories/SportsProUnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿namespace SportsPro.Data.DataLayer.Repositories
+{
+    public class SportsProUnitOfWork : ISportsProUnitOfWork
+    {
+        private readonly SportsProContext context;
+
+        public IRepository<Customer> Customers { get; private set; }
+        public IRepository<Incident> Incidents { get; private set; }
+        public IRepository<Product> Products { get; private set; }
+        public IRepository<Registration> Registrations { get; private set; }
+        public IRepository<Technician> Technicians { get; private set; }
+        public IRepository<Country> Countries { get; private set; }
+  
+        public SportsProUnitOfWork(SportsProContext context)
+        {
+            this.context = context;
+            Customers = new Repository<Customer>(context);
+            Incidents = new Repository<Incident>(context);
+            Products = new Repository<Product>(context);
+            Registrations = new Repository<Registration>(context);
+            Technicians = new Repository<Technician>(context);
+            Countries = new Repository<Country>(context);
+        }
+
+        public void Save() => context.SaveChanges();
+    }
+}
