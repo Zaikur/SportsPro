d87f1f1e239eb9336b475c8e7ff203b5fb55a5ef
Quinton Nelson <124801789+Zaikur@users.noreply.github.com>
2024-04-14 13:52:32 -0500

    Created new SportsProContext file

--- SportsPro/Data/DataLayer/SportsProContext.cs
+++ SportsPro/Data/DataLayer/SportsProContext.cs
@@ -1,6 +1,26 @@
-﻿namespace SportsPro.Data.DataLayer
+﻿using Microsoft.EntityFrameworkCore;
+using SportsPro.Data.Configuration;
+
+namespace SportsPro.Data.DataLayer
 {
-    public class SportsProContext
+    public class SportsProContext : DbContext
     {
+        public SportsProContext(DbContextOptions<SportsProContext> options) : base(options) { }
+
+        public DbSet<Models.Customer> Customers { get; set; }
+        public DbSet<Models.Incident> Incidents { get; set; }
+        public DbSet<Models.Product> Products { get; set; }
+        public DbSet<Models.Registration> Registrations { get; set; }
+        public DbSet<Models.Technician> Technicians { get; set; }
+        public DbSet<Models.Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CountryConfig());
+            modelBuilder.ApplyConfiguration(new CustomerConfig());
+            modelBuilder.ApplyConfiguration(new IncidentConfig());
+            modelBuilder.ApplyConfiguration(new ProductConfig());
+            modelBuilder.ApplyConfiguration(new TechnicianConfig());
+        }
     }
 }
